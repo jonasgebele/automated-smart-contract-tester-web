72b5934d400a53510541951762afce703aa41b26
Jonas Gebele <ge69cas@mytum.de>
2024-08-10 14:07:20 +0200

    Update vuetify.js

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,16 +1,11 @@
-import 'vuetify/styles';
-import '@fortawesome/fontawesome-free/css/all.css';
-import '@mdi/font/css/materialdesignicons.css';
-
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
-import * as dataTableComponents from 'vuetify/labs/VDataTable';
 import * as directives from 'vuetify/directives';
 import { aliases, fa } from 'vuetify/iconsets/fa';
 import { mdi } from 'vuetify/iconsets/mdi';
 
 const vuetify = createVuetify({
-  components: { ...components, ...dataTableComponents },
+  components: { ...components },
   directives,
   theme: {
     defaultTheme: 'theme',
